7aeee7df7ea12cdbdc88b296247224ab6ef7e1c1
beepscore <support@beepscore.com>
2013-07-30 00:22:58 -0700

    Get image from camera.
    Reference: Apple Documentation / Camera Programming Topics for iOS / Taking Pictures and Movies

--- PhotoSnap/BSViewController.h
+++ PhotoSnap/BSViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BSViewController : UIViewController
+@interface BSViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @end
--- PhotoSnap/BSViewController.m
+++ PhotoSnap/BSViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "BSViewController.h"
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface BSViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *imageLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation BSViewController
@@ -20,10 +22,92 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - memory management
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)presentCamera:(id)sender
+{
+    [self startCameraControllerFromViewController:self usingDelegate:self];
+}
+
+// Reference Apple Documentation / Camera Programming Topics for iOS / Taking Pictures and Movies
+- (BOOL) startCameraControllerFromViewController: (UIViewController*) controller
+                                   usingDelegate: (id <UIImagePickerControllerDelegate,
+                                                   UINavigationControllerDelegate>) delegate
+{
+    
+    if (([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera] == NO)
+        || (delegate == nil)
+        || (controller == nil))
+    {
+        return NO;
+    }
+    
+    UIImagePickerController *cameraUI = [[UIImagePickerController alloc] init];
+    cameraUI.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    // Displays a control that allows the user to choose picture or
+    // movie capture, if both are available:
+//    cameraUI.mediaTypes =
+//    [UIImagePickerController availableMediaTypesForSourceType:
+//     UIImagePickerControllerSourceTypeCamera];
+    
+    // Hides the controls for moving & scaling pictures, or for
+    // trimming movies. To instead show the controls, use YES.
+    cameraUI.allowsEditing = NO;
+    
+    cameraUI.delegate = delegate;
+    
+    // as side effect this should set cameraUI parentview controller?
+//    [controller presentViewController:cameraUI
+//                             animated:YES
+//                           completion:nil];
+    [self presentViewController:cameraUI
+                             animated:YES
+                           completion:nil];
+    return YES;
+}
+
+#pragma mark - UIImagePickerControllerDelegate Methods
+
+// For responding to the user tapping Cancel.
+- (void) imagePickerControllerDidCancel: (UIImagePickerController *) picker {
+    
+    //[[picker parentViewController] dismissViewControllerAnimated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// For responding to the user accepting a newly-captured picture or movie
+- (void) imagePickerController: (UIImagePickerController *) picker
+ didFinishPickingMediaWithInfo: (NSDictionary *) info {
+    
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    UIImage *originalImage, *editedImage, *imageToSave;
+    
+    // Handle a still image capture
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeImage, 0) == kCFCompareEqualTo)
+    {
+        editedImage = (UIImage *) [info objectForKey:
+                                   UIImagePickerControllerEditedImage];
+        originalImage = (UIImage *) [info objectForKey:
+                                     UIImagePickerControllerOriginalImage];
+        
+        if (editedImage)
+        {
+            imageToSave = editedImage;
+        } else
+        {
+            imageToSave = originalImage;
+        }
+        self.imageView.image = imageToSave;
+    }
+   
+//    [[picker parentViewController] dismissViewControllerAnimated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
